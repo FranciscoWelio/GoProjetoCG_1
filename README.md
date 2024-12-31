a768b9c5ba3264e51dbfe4ed7dfed07f8eababaa
Francisco Welio <fwelio.dev@gmail.com>
2024-12-31 16:10:23 -0300

    feat/ Separação de objetos

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # GoProjetoCG_1
-GoProjetoCG 1
+
+---
+
+## Projeto de Computação Gráfica em Go
+
+1. Algoritmos Básico de Desenho
+	- Desenho de reta com algoritmo DDA
+	- Desenho de reta com algoritmo Ponto Médio
+	- Desenho de circunferência método trigonométrico
+	- Desenho de circunferência método Simetria de Grau 8
+	- Desenho de circunferência Algoritmo Ponto Médio
+	- Desenho de elipse Algoritmo Ponto Médio
+2. Matrizes de Composição
+	- Matriz Translação 2D e 3D
+	- Matriz Escala
+	- Matriz Rotação
+		- Em X
+		- Em Y
+		- Em (0,0)
+		- Em 45º
+		- Em Reta qualquer
+	- Matriz Reflexão
+		- Em X
+		- Em Y
+		- Na Origem
+	- Matriz Cisalhamento
+		- Em X
+		- Em Y
+		- Em XY
+3. Operação com ViewPort e Recorte
+	- Algoritmo de Corhen Southerland
+	- Aplicações 1 e 2 em ViewPort
+4. Batimento de Coração
---
+++ button.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+// CreateButtons cria e retorna o frame com os botões
+func CreateButtons(app fyne.App) fyne.CanvasObject {
+	label := widget.NewLabel("Controles:")
+	aux := false
+	var DDA *widget.Button
+	DDA = widget.NewButton("Reta DDA", func() {
+		if aux {
+			DDA.SetText("Reta Ponto Médio")
+		} else {
+			DDA.SetText("Em desenvolvimento")
+		}
+		aux = !aux
+	})
+
+	aux1 := false
+	var PM *widget.Button
+	PM = widget.NewButton("Reta Ponto Médio", func() {
+		if aux1 {
+			PM.SetText("Reta Ponto Médio")
+		} else {
+			PM.SetText("Em desenvolvimento")
+		}
+		aux1 = !aux1
+	})
+
+	exitButton := widget.NewButton("Sair", func() {
+		app.Quit()
+	})
+
+	// Frame da esquerda
+	return container.NewVBox(
+		label,
+		DDA,
+		PM,
+		exitButton,
+		layout.NewSpacer(), // Adiciona espaço flexível
+	)
+}
