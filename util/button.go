2a4481f529a8b4fa866bc3e55c29e7ddbdcdc5a3
Francisco Welio <fwelio.dev@gmail.com>
2025-01-06 22:33:03 -0300

    fix/dda corrigido para aperecer no frame principal

--- util/button.go
+++ util/button.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -8,17 +10,25 @@ import (
 )
 
 // CreateButtons cria e retorna o frame com os botões
-func CreateButtons(app fyne.App) fyne.CanvasObject {
+func CreateButtons(app fyne.App, canvasContent *fyne.Container, width, height int) fyne.CanvasObject {
+	x0 := widget.NewEntry()
+	x0.SetPlaceHolder("X")
+	y0 := widget.NewEntry()
+	y0.SetPlaceHolder("Y")
+	xEnd := widget.NewEntry()
+	xEnd.SetPlaceHolder("X Final")
+	yEnd := widget.NewEntry()
+	yEnd.SetPlaceHolder("Y Final")
 	label := widget.NewLabel("Controles:")
-	aux := false
-	var DDA *widget.Button
-	DDA = widget.NewButton("Reta DDA", func() {
-		if aux {
-			DDA.SetText("Reta Ponto Médio")
-		} else {
-			DDA.SetText("Em desenvolvimento")
-		}
-		aux = !aux
+	gridInput := container.New(layout.NewGridLayout(2), x0, y0, xEnd, yEnd)
+	var ddaButon *widget.Button
+	ddaButon = widget.NewButton("Reta DDA", func() {
+		x, _ := strconv.ParseFloat(x0.Text, 64)
+		y, _ := strconv.ParseFloat(y0.Text, 64)
+		XE, _ := strconv.ParseFloat(xEnd.Text, 64)
+		YE, _ := strconv.ParseFloat(yEnd.Text, 64)
+
+		DDA(x, y, XE, YE, canvasContent, width, height)
 	})
 
 	aux1 := false
@@ -39,7 +49,8 @@ func CreateButtons(app fyne.App) fyne.CanvasObject {
 	// Frame da esquerda
 	return container.NewVBox(
 		label,
-		DDA,
+		ddaButon,
+		gridInput,
 		PM,
 		exitButton,
 		layout.NewSpacer(), // Adiciona espaço flexível
