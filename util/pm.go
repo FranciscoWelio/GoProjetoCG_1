3b6c7dde48e9432b097c5bc10be9c21b34b35801
Francisco Welio <fwelio.dev@gmail.com>
2025-01-14 22:53:45 -0300

    fix/todos oitantes PM funcionando

--- util/pm.go
+++ util/pm.go
@@ -44,6 +44,169 @@ func PontoMedio(x0, y0, xEnd, yEnd float64, canvasContent *fyne.Container, width
 			canvasContent.Add(pixel)
 		}
 		canvasContent.Refresh()
+	} else if dy >= dx && x0 <= xEnd && y0 <= yEnd {
+		fmt.Println("2º Oitante")
+		ds := 2*dx - dy
+		incE := 2 * dx
+		incNE := 2 * (dx - dy)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for y < yEnd {
+			y += 1
+			if ds < 0 {
+				ds += incE
+			} else {
+				x += 1
+				ds += incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dy >= dx && x0 >= xEnd && y0 <= yEnd {
+		fmt.Println("3º Oitante")
+		ds := 2*dx - dy
+		incE := 2 * dx
+		incNE := 2 * (dx - dy)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for x > xEnd {
+			y += 1
+			if ds < 0 {
+				ds += incE
+			} else {
+				x -= 1
+				ds += incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dx >= dy && x0 >= xEnd && y0 <= yEnd {
+		fmt.Println("4º Oitante")
+		ds := 2*dy - dx
+		incE := 2 * dy
+		incNE := 2 * (dy - dx)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for x > xEnd {
+			x -= 1
+			if ds < 0 {
+				ds += incE
+			} else {
+				y += 1
+				ds += incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dx >= dy && x0 >= xEnd && y0 >= yEnd {
+		fmt.Println("5º Oitante")
+		ds := 2*-dy + dx
+		incE := 2 * -dy
+		incNE := 2 * (-dy + dx)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for x > xEnd {
+			x -= 1
+			if ds < 0 {
+				ds += -incE
+			} else {
+				y -= 1
+				ds += -incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dy >= dx && x0 >= xEnd && y0 >= yEnd {
+		fmt.Println("6º Oitante")
+		dx, dy := dy, dx
+		ds := 2*-dy + dx
+		incE := 2 * -dy
+		incNE := 2 * (-dy + dx)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for y > yEnd {
+			y -= 1
+			if ds < 0 {
+				ds += -incE
+			} else {
+				x -= 1
+				ds += -incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dy >= dx && xEnd >= x0 && y0 >= yEnd {
+		fmt.Println("7º Oitante")
+		dx, dy := dy, dx
+		ds := 2*-dy + dx
+		incE := 2 * -dy
+		incNE := 2 * (-dy + dx)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for y > yEnd {
+			y -= 1
+			if ds < 0 {
+				ds += -incE
+			} else {
+				x += 1
+				ds += -incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
+	} else if dx >= dy && x0 <= xEnd && y0 >= yEnd {
+		fmt.Println("8º Oitante")
+		ds := 2*-dy + dx
+		incE := 2 * -dy
+		incNE := 2 * (-dy + dx)
+		x, y := x0, y0
+		pixel := canvas.NewRectangle(color.Black)
+		pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+		pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+		canvasContent.Add(pixel)
+		for x < xEnd {
+			x += 1
+			if ds < 0 {
+				ds += -incE
+			} else {
+				y -= 1
+				ds += -incNE
+			}
+			pixel := canvas.NewRectangle(color.Black)
+			pixel.Resize(fyne.NewSize(1, 1)) // Tamanho do pixel
+			pixel.Move(fyne.NewPos(float32(centerX+x), float32(centerY-y)))
+			canvasContent.Add(pixel)
+		}
 	}
 
 }
