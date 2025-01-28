383a0d02dbb3ab83e1ed8ce96bdc420f8529bc69
Francisco Welio <fwelio.dev@gmail.com>
2025-01-28 11:31:43 -0300

    fix/validação de valores

--- err/validateerror.go
+++ err/validateerror.go
@@ -8,13 +8,14 @@ import (
 	"fyne.io/fyne/v2"
 )
 
-func ValidateInput(variavel string, nome string, myJanela fyne.Window) (float64, error) {
-	if variavel == "" {
-		myDialog := dialog.NewCustomDialog(myJanela, "Erro nos valores "+nome)
+func ValidateInput(variavel *string, nome string, myJanela fyne.Window) (float64, error) {
+	if *variavel == "" {
+		myDialog := dialog.NewCustomDialog(myJanela, "Erro nos valores "+nome+"\nO valores serão transformados em \"0\"")
 		myDialog.Show()
+		*variavel = "0"
 		return 0, fmt.Errorf("o campo %s está vazio", nome)
 	}
-	vari, err := strconv.ParseFloat(variavel, 64)
+	vari, err := strconv.ParseFloat(*variavel, 64)
 	if err != nil {
 		return 0, fmt.Errorf("O campo %s é inválido: %v", nome, err)
 	}
